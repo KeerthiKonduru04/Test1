38e571ec407e7789a71381100fb0594339472be0
KeerthiKonduru04 <keerthivempalli04@gmail.com>
2025-02-19 11:24:51 +0530

    Second

--- src/org/kerala/Kerala.java
+++ src/org/kerala/Kerala.java
@@ -5,7 +5,7 @@ import org.andhrapradesh.Andhrapradesh;
 public class Kerala extends Andhrapradesh{
 	public void malayalam()
 	{
-		System.out.println("Language is Malayalam");
+		System.out.println("Language ierrs Malayalam");
 	}
 	public static void main(String[] args) {
 		Kerala k= new Kerala();
